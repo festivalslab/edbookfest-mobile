89c994fedfbfb0194e990efc25ffbebb4787b996
James Newbery <james@tinnedfruit.com>
2011-06-08 07:11:39 +0100

    Initial rails 3.1 app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Edinburgh International Book Festival mobile website
+
+This is the application behind the Edinburgh International Book Festival 2011 mobile website.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdbookfestMobile::Application.config.secret_token = '25f8ddb91342bac11e49f44ae11eb5e82fcf122ddc3166b4bd015eac8baa9a4bb316d70eaf5968cdb128723e7f7df6f4f41ae9fab6701d2a0e93c31318ba2e9d'
