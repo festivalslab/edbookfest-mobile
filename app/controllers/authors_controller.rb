0793a86e99e61fcf2256c4ced7620577f6862f70
James Newbery <james@tinnedfruit.com>
2011-06-23 07:58:50 +0100

    Preview theme variations for events, authors & books

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,5 @@
+class AuthorsController < ApplicationController
+  def index
+    @bodyClass = "authors"
+  end
+end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,5 @@
+class BooksController < ApplicationController
+  def index
+    @bodyClass = "books"
+  end
+end
