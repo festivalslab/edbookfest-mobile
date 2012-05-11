festivalslab/edbookfest-mobile

commit 92a1ce803f0b4dabe71d931aea0963d27a9116c7

Show a "coming soon" message before events are available
If there are no events in the database (most likely because the
festival has not yet launched) show a "coming soon" message
page on every request rather than serve an empty site. Automatically
disabled as soon as events are found.

app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   http_basic_authenticate_with :name => ENV["HTTP_USERNAME"], :password => ENV["HTTP_PASSWORD"], :realm => "EIFB mobile site" if ENV["HTTP_AUTH_ACTIVE"]
-  before_filter :page_cache
+  before_filter :page_cache, :check_launched
   
 protected
 
@@ -21,6 +21,10 @@ protected
     cache_time = Rails.application.config.cache_times[type] * 60
     cache_header = cache_time > 0 ? "public, max-age=#{cache_time}" : "no-cache"
     response.headers['Cache-Control'] = cache_header
+  end
+  
+  def check_launched
+    redirect_to coming_url unless (Event.all.count > 0)
   end
 
 end

app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   helper CalendarHelper
   before_filter :set_theme, :set_section
+  skip_before_filter :check_launched, :only => [:coming]
   layout :set_layout
   
   def index
@@ -31,6 +32,12 @@ class EventsController < ApplicationController
     @title = "Events calendar"
     @start_date = Festival.start_date
     @end_date = Festival.end_date
+  end
+  
+  def coming
+    redirect_to calendar_url unless (Event.all.count == 0)
+    @title = "Coming Soon"
+    @section = "Coming Soon"
   end
   
   def show

app/views/events/coming.html.haml
@@ -1,0 +1,17 @@
+%hgroup
+  .main-title
+    %h3
+      Edinburgh International Book Festival
+
+.content
+  .description
+    %h4
+      We’ll be launching our 2012 programme at noon on Thursday 21 June.
+    %p
+      Join us then, when you’ll be able to browse the full line-up of events, 
+      and find out what’s in store.
+    %p
+      In the meantime you can read commissioned writing, listen to recordings of 
+      past events and find out more about the Book Festival
+      %a.desktop-site{:href => "http://www.edbookfest.co.uk", :"data-remote" => "", :"data-analytics-action" => "Desktop site"} 
+        on our main website.

config/routes.rb
@@ -1,6 +1,7 @@
 EdbookfestMobile::Application.routes.draw do
   
   match "events/calendar" => "events#calendar", :as => :calendar
+  match "coming" => "events#coming", :as => :coming
   match "events/calendar/:date(/:type)" => "events#index", :as => :listings, :constraints => { :date => /\d{4}-\d{2}-\d{2}/, :type => /(Adult|Children)/ }
   
   resources :events, :only => [:index, :show]

features/coming_soon.feature
@@ -1,0 +1,31 @@
+Feature: Coming Soon page
+  In order to understand why there are no events listed
+  As a festival-goer
+  I want to see when the programme will be launched
+  
+  @nowebmock @wip
+  Scenario Outline: Before the programme is launched, all pages redirect to the coming soon page
+    Given today is 01/05/2011
+    And there are no events in the database
+    When I go to <requested_page>
+    Then I should be on the coming soon page
+    
+    Examples:
+      | requested_page                   |
+      | the home page                    |
+      | the calendar page                |
+      | the listings page for 13/08/2011 |
+  
+  @nowebmock @wip
+  Scenario: The coming soon page explains to the user that the programme has not yet launched
+    Given today is 01/05/2011
+    And there are no events in the database
+    When I go to the coming soon page
+    Then the section title should be "Coming Soon"
+
+  @nowebmock @wip
+  Scenario: If the user arrives on the coming soon page once events are available they are taken to the calendar
+    Given today is 01/05/2011
+    And there is 1 event for 13/08/2011 starting at 09:00
+    When I go to the coming soon page
+    Then I should be on the calendar page

features/step_definitions/application_steps.rb
@@ -32,7 +32,7 @@ Then /^the page heading should be "([^\"]*)"$/ do |text|
   page.should have_css("h3", :text => text)
 end
 
-Then /^I should be on the (\w+) page$/ do |page|
+Then /^I should be on the (\w+|coming soon) page$/ do |page|
   current_path = URI.parse(current_url).path
   current_path.should == path_to("the #{page} page")
 end

features/step_definitions/event_listings_steps.rb
@@ -1,3 +1,7 @@
+Given /^there are no events in the database$/ do
+  Event.all.count.should == 0
+end
+
 Given /^there (?:is|are) (\d+) (child|adult)?\s?(sold out)?\s?events? for (\d+)\/(\d+)\/(\d+) starting at (\d+):(\d+)$/ do |count, type, sold_out, day, month, year, hours, minutes|
   event_type = (type == "child") ? "Children" : "Adult"
   date_pattern = "#{year}-#{month}-#{day}T#{hours}:#{minutes}:00+01:00"

features/support/paths.rb
@@ -13,6 +13,9 @@ module NavigationHelpers
       
     when /^the calendar page$/
       calendar_path
+    
+    when /^the coming soon page$/
+      coming_path
       
     when /^the listings page for (\d+)\/(\d+)\/(\d+)$/
       listings_path("#{$3}-#{$2}-#{$1}")

commit 7d4c7599e6db11f1bdd0bbb47acb98d9349043d2

Fix controller specs broken by 92a1ce803f
Introduced Festival.is_launched so that coming soon logic can be stubbed
for other controller specs which were broken by the last commit. Also
added caching logic.

app/controllers/application_controller.rb
@@ -24,7 +24,10 @@ protected
   end
   
   def check_launched
-    redirect_to coming_url unless (Event.all.count > 0)
+    if ! Festival.is_launched
+      set_cache(:home)
+      redirect_to coming_url
+    end
   end
 
 end

app/controllers/events_controller.rb
@@ -35,7 +35,12 @@ class EventsController < ApplicationController
   end
   
   def coming
-    redirect_to calendar_url unless (Event.all.count == 0)
+    if Festival.is_launched
+      set_cache(:default)
+      redirect_to calendar_url
+      return
+    end
+    set_cache(:home)
     @title = "Coming Soon"
     @section = "Coming Soon"
   end

app/models/Festival.rb
@@ -13,4 +13,8 @@ class Festival
   def self.date_in_festival(date)
     date >= @@start_date && date <= @@end_date
   end
+  
+  def self.is_launched
+    (Event.all.count > 0)
+  end
 end

spec/controllers/articles_controller_spec.rb
@@ -8,6 +8,7 @@ describe ArticlesController do
   before(:each) do
     Author.stub(:find_by_eibf_id).and_return author
     author.stub(:full_name).and_return("Joe Bloggs")
+    Festival.stub(:is_launched).and_return true
   end
 
   describe "GET 'index'" do

spec/controllers/authors_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe AuthorsController do
+  before(:each) do
+    Festival.stub(:is_launched).and_return true
+  end
+
   
   describe "GET 'show'" do
     let(:expected_theme) { "authors" }

spec/controllers/books_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe BooksController do
+  before(:each) do
+    Festival.stub(:is_launched).and_return true
+  end
+  
   describe "GET 'show'" do
     let(:book) { mock_model(Book).as_null_object }
     let(:amazon_book) { double("AmazonBook").as_null_object }

spec/controllers/events_controller_spec.rb
@@ -11,6 +11,7 @@ describe EventsController do
     Festival.stub(:start_date).and_return(fest_start)
     Festival.stub(:end_date).and_return(fest_end)
     Festival.stub(:date_in_festival).and_return(true)
+    Festival.stub(:is_launched).and_return true
   end
   
   describe "setting layout" do
@@ -65,6 +66,22 @@ describe EventsController do
   end
   
   describe "GET 'index'" do    
+    context "when the festival has not launched" do
+      before(:each) do
+        Festival.stub(:is_launched).and_return(false)
+      end
+        
+      it "redirects to the coming soon page" do
+          get :index
+          response.should redirect_to coming_url
+      end
+        
+      it "sets the cache headers to a minute" do
+        get :index
+        response.headers['Cache-Control'].should == 'public, max-age=60'
+      end
+    end
+    
     context "when a date is provided" do
       context "and when the date is during the festival" do
         let(:date) { Date.new(2011,8,13) }
@@ -306,6 +323,56 @@ describe EventsController do
         @book.stub(:amazon_lookup).and_return nil
         get :show, :id => "1234-title"
         assigns[:amazon_books].should eq []
+      end
+    end
+  end
+  
+  describe "GET 'coming'" do
+    context "When the festival has not launched" do
+      before(:each) do
+        Festival.stub(:is_launched).and_return false
+      end
+      
+      it "should be successful" do
+        get :coming
+        response.should be_success
+      end
+      
+      it "sets the cache headers to a minute" do
+        get :calendar
+        response.headers['Cache-Control'].should == 'public, max-age=60'
+      end
+      
+      it "assigns @theme" do
+        get :coming
+        assigns[:theme].should eq(expected_theme)
+      end
+      
+      it "assigns @section" do
+        get :coming
+        assigns[:section].should eq("Coming Soon")
+      end
+      
+      it "assigns @title" do
+        get :coming
+        assigns[:title].should eq("Coming Soon")
+      end
+      
+    end
+    
+    context "When the festival has launched" do
+      before(:each) do
+        Festival.stub(:is_launched).and_return true
+      end
+      
+      it "redirects to the calendar" do
+        get :coming
+        response.should redirect_to calendar_url
+      end
+      
+      it "sets the cache headers to an hour" do
+        get :coming
+        response.headers['Cache-Control'].should == 'public, max-age=3600'
       end
     end
   end

spec/models/festival_spec.rb
@@ -28,4 +28,19 @@ describe Festival do
       Festival.date_in_festival(date).should_not be_true
     end
   end
+  
+  describe "#is_launched" do
+    context "when there are events" do
+    end
+      it "returns true" do
+        Event.create
+        Festival.is_launched.should be_true
+      end
+    
+    context "when there are no events" do
+      it "returns false" do
+        Festival.is_launched.should_not be_true
+      end
+    end
+  end
 end